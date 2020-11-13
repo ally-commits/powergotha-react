a228086c7d095b7585a3cd032523bac2d43c799d
AlwinCrasta <alwyncrasta000@gmail.com>
2020-11-13 20:42:05 +0530

    init

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
-// export const backendUrl = "https://yopaan-node.codedeployment.tk/api"   
 
-export const backendUrl = "http://localhost:9000/api"   
+export const backendUrl = "https://yopaan-node.codedeployment.tk/api"   
+
+// export const backendUrl = "http://localhost:9000/api"   
